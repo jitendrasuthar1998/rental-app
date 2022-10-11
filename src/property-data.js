5bb8aaba91453f1aeadd22635ed01b5cb5d0672b
Jitendra Suthar <jitendrasuthar995@gmail.com>
2022-10-12 02:34:35 +0530

    completed rental-app user interface

---
+++ src/property-data.js
@@ -0,0 +1,329 @@
+const property = {
+    rent:500,
+    image:"https://images.pexels.com/photos/106399/pexels-photo-106399.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    popular: false,
+    location:"394 Manchester Blvd, Fl",
+    propertyType:"houses",
+    moveIn:"immediate",
+    beds:4,
+    bathrooms: 2,
+    size:"5x6",
+    name:"Palm Harbor",
+    city:"Florida",
+    state: "USA"
+}
+
+const property1 = {
+    rent:2700,
+    image:"https://images.pexels.com/photos/186077/pexels-photo-186077.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    popular: true,
+    location:"2821 Lake Sevilla Palm Harbor, TX",
+    propertyType:"houses",
+    moveIn:"immediate",
+    beds:4,
+    bathrooms: 2,
+    size:"6x7",
+    name:"Beverly Springfield",
+    city:"Texas",
+    state: "USA"
+}
+
+const property2 = {
+    rent:4550,
+    image:"https://images.pexels.com/photos/323780/pexels-photo-323780.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    popular: true,
+    location:"909 Woodland St. Mchigan, IN",
+    propertyType:"villa",
+    moveIn:"in 2 day",
+    beds:4,
+    bathrooms: 3,
+    size:"8x10",
+    name:"Faulkner Ave",
+    city:"Mchigan",
+    state: "USA"
+}
+
+const property3 = {
+    rent:1500,
+    image:"https://images.pexels.com/photos/1396122/pexels-photo-1396122.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    popular: true,
+    location:"6649 N Blue Gum St",
+    propertyType:"apartment",
+    moveIn:"immediate",
+    beds:4,
+    bathrooms: 2,
+    size:"6x6",
+    name:"Brigadier House",
+    city:"New York",
+    state: "USA"
+}
+
+const property4 = {
+    rent:2500,
+    image:"https://images.pexels.com/photos/164558/pexels-photo-164558.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    popular: true,
+    location:"5 Boston Ave",
+    propertyType:"houses",
+    moveIn:"immediate",
+    beds:4,
+    bathrooms: 2,
+    size:"6x6",
+    name:"Nature House",
+    city:"New York",
+    state: "USA"
+}
+
+
+
+const property5 = {
+    rent:2500,
+    image:"https://images.pexels.com/photos/209296/pexels-photo-209296.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    popular: true,
+    location:"5 Boston Ave",
+    propertyType:"houses",
+    moveIn:"in 2-5 days",
+    beds:4,
+    bathrooms: 2,
+    size:"6x6",
+    name:"Kanaught Place",
+    city:"Florida",
+    state: "USA"
+}
+
+const property6 = {
+    rent:2100,
+    image:"https://images.pexels.com/photos/209315/pexels-photo-209315.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    popular: false,
+    location:"2742 Distribution Way, Fl",
+    propertyType:"houses",
+    moveIn:"in 1 day",
+    beds:4,
+    bathrooms: 2,
+    size:"6x6",
+    name:"Sharma's House",
+    city:"Florida",
+    state: "USA"
+}
+
+const property7 = {
+    rent:2270,
+    image:"https://images.pexels.com/photos/221024/pexels-photo-221024.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    popular: true,
+    location:"2742 Distribution Way, Fl",
+    propertyType:"apartment",
+    moveIn:"in 1 day",
+    beds:4,
+    bathrooms: 2,
+    size:"6x6",
+    name:"Laxmi Nivash",
+    city:"Florida",
+    state: "USA"
+}
+const property8 = {
+    rent:3500,
+    image:"https://images.pexels.com/photos/463996/pexels-photo-463996.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    popular: true,
+    location:"5 Tomahawk Dr, Fl",
+    propertyType:"apartment",
+    moveIn:"in next week",
+    beds:4,
+    bathrooms: 2,
+    size:"6x6",
+    name:"Sharma's House",
+    city:"Texas",
+    state: "USA"
+}
+
+const property9 = {
+    rent:4000,
+    image:"https://images.pexels.com/photos/210617/pexels-photo-210617.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    popular: true,
+    location:"2742 Distribution Way, CA",
+    propertyType:"villa",
+    moveIn:"in next week",
+    beds:4,
+    bathrooms: 2,
+    size:"7x9",
+    name:"Monk House",
+    city:"California",
+    state: "USA"
+}
+
+const property10 = {
+    rent:4600,
+    image:"https://images.pexels.com/photos/2850472/pexels-photo-2850472.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    popular: true,
+    location:"209 Decker Dr, CA",
+    propertyType:"villa",
+    moveIn:"in next week",
+    beds:4,
+    bathrooms: 2,
+    size:"10x9",
+    name:"The Big Boss House",
+    city:"California",
+    state: "USA"
+}
+
+const property11 = {
+    rent:4900,
+    image:"https://images.pexels.com/photos/164522/pexels-photo-164522.jpeg?auto=compress&cs=tinysrgb&w=1600",
+    popular: true,
+    location:"128 Bransten Rd, CA",
+    propertyType:"villa",
+    moveIn:"in next week",
+    beds:7,
+    bathrooms: 4,
+    size:"13x12",
+    name:"The Beauty Place",
+    city:"California",
+    state: "USA"
+}
+
+const property12 = {
+    rent:2830,
+    image:"https://images.pexels.com/photos/259685/pexels-photo-259685.jpeg?auto=compress&cs=tinysrgb&w=1600",
+    popular: false,
+    location:"2742 Distribution Way, CA",
+    propertyType:"villa",
+    moveIn:"in 1 day",
+    beds:4,
+    bathrooms: 2,
+    size:"10x9",
+    name:"The Beauty Place",
+    city:"California",
+    state: "USA"
+}
+
+const property13 = {
+    rent:4830,
+    image:"https://images.pexels.com/photos/2893177/pexels-photo-2893177.jpeg?auto=compress&cs=tinysrgb&w=1600",
+    popular: true,
+    location:"2 Lighthouse Ave, CA",
+    propertyType:"villa",
+    moveIn:"in 1 day",
+    beds:6,
+    bathrooms: 3,
+    size:"10x9",
+    name:"The Lighthouse",
+    city:"California",
+    state: "USA"
+}
+
+
+
+const property14 = {
+    rent:870,
+    image:"https://images.pexels.com/photos/210538/pexels-photo-210538.jpeg?auto=compress&cs=tinysrgb&w=1600",
+    popular: true,
+    location:"209 Decker Dr, Fl",
+    propertyType:"apartment",
+    moveIn:"in 1 day",
+    beds:4,
+    bathrooms: 2,
+    size:"6x6",
+    name:"The Grace House",
+    city:"Florida",
+    state: "USA"
+}
+
+const property15 = {
+    rent:3870,
+    image:"https://images.pexels.com/photos/534228/pexels-photo-534228.jpeg?auto=compress&cs=tinysrgb&w=1600",
+    popular: false,
+    location:"71 San Mateo Ave, Tx",
+    propertyType:"apartment",
+    moveIn:"in 1 day",
+    beds:4,
+    bathrooms: 2,
+    size:"6x6",
+    name:"King's Palace",
+    city:"Texas",
+    state: "USA"
+}
+
+const property16 = {
+    rent:1110,
+    image:"https://images.pexels.com/photos/2510067/pexels-photo-2510067.jpeg?auto=compress&cs=tinysrgb&w=1600",
+    popular: false,
+    location:"18 Fountain St, Tx",
+    propertyType:"houses",
+    moveIn:"in next week",
+    beds:4,
+    bathrooms: 2,
+    size:"3x6",
+    name:"Kunti Nivash",
+    city:"Texas",
+    state: "USA"
+}
+
+const property17 = {
+    rent:5230,
+    image:"https://images.pexels.com/photos/280216/pexels-photo-280216.jpeg?auto=compress&cs=tinysrgb&w=1600",
+    popular: true,
+    location:"59 N Groesbeck Hwy, Tx",
+    propertyType:"houses",
+    moveIn:"in 1 day",
+    beds:20,
+    bathrooms: 5,
+    size:"30x60",
+    name:"The Pandav's Palace",
+    city:"Texas",
+    state: "USA"
+}
+
+const property18 = {
+    rent:2360,
+    image:"https://images.pexels.com/photos/2416472/pexels-photo-2416472.jpeg?auto=compress&cs=tinysrgb&w=1600",
+    popular: false,
+    location:"71 San Mateo Ave, Ny",
+    propertyType:"houses",
+    moveIn:"immediate",
+    beds:4,
+    bathrooms: 2,
+    size:"6x6",
+    name:"The Green House",
+    city:"New York",
+    state: "USA"
+}
+
+const property19 = {
+    rent:1450,
+    image:"https://images.pexels.com/photos/1105754/pexels-photo-1105754.jpeg?auto=compress&cs=tinysrgb&w=1600",
+    popular: false,
+    location:"17 Morena Blvd, Ny",
+    propertyType:"houses",
+    moveIn:"immediate",
+    beds:4,
+    bathrooms: 2,
+    size:"6x6",
+    name:"The Birla's House",
+    city:"New York",
+    state: "USA"
+}
+
+const property20 = {
+    rent:2535,
+    image:"https://images.pexels.com/photos/176860/pexels-photo-176860.jpeg?auto=compress&cs=tinysrgb&w=1600",
+    popular: true,
+    location:"5 Boston Ave",
+    propertyType:"houses",
+    moveIn:"in next week",
+    beds:6,
+    bathrooms: 3,
+    size:"7x6",
+    name:"Nick & Jons House",
+    city:"New York",
+    state: "USA"
+}
+
+const propertyData = [
+    property, property1, property2, property3, property4, property5, property6, property7, property8, property9, property10, property11, property12, property13, property14, property15, property16, property17, property18, property19, property20
+]
+
+
+const propertyType = ["houses", "apartment", "villa"]
+const moveIn = ["immediate", "in 1 day", "in 2-5 days", "in next week"]
+const price = [500, 5000]
+const city = ["California", "New York", "Texas", "Florida"]
+
+export default propertyData;
